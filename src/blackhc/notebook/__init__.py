eebf33672db0fc14872d5419fd31d24fd067cd5b
Andreas Kirsch <blackhc@gmail.com>
2018-02-22 18:14:58 +0000

    Add support for git and better support for cookiecutter projects.

--- src/blackhc/notebook/__init__.py
+++ src/blackhc/notebook/__init__.py
@@ -1,28 +1,61 @@
 import sys
 import os
 
-_notebook_path = None
+_project_dir = None
 
 
-def set_notebook_path(notebook_path):
-    global _notebook_path
-    if _notebook_path:
-        old_src_path = get_src_path()
+def set_project_dir(project_dir):
+    global _project_dir
+    if _project_dir:
+        old_src_path = get_src_path(_project_dir)
         if old_src_path in sys.path:
             sys.path.remove(old_src_path)
-    _notebook_path = notebook_path
-    src_path = get_src_path(_notebook_path)
+    _project_dir = project_dir
+    src_path = get_src_path(_project_dir)
     if src_path and src_path not in sys.path:
         sys.path.append(src_path)
+        print('Appended %s to paths' % src_path)
+
+    if _project_dir:
+        os.chdir(project_dir)
+        print('Switched to directory %s' % project_dir)
 
 
-def get_src_path(notebook_path):
-    p = notebook_path
-    while not p.ismount():
-        if p.basename() == 'notebooks':
-            return p.dirname().joinpath('src')
-        p = p.dirname()
+def infer_and_set_project_dir():
+    set_project_dir(get_git_working_dir(os.getcwd()) or
+                    get_cookiecutter_project_path(os.getcwd()))
+
+
+# Inspired by https://stackoverflow.com/questions/19687394/python-script-to-determine-if-a-directory-is-a-git-repository
+def get_git_working_dir(path):
+    try:
+        import git
+    except ImportError:
+        return None
+
+    try:
+        return git.Repo(path, search_parent_directories=True).working_tree_dir
+    except git.exc.InvalidGitRepositoryError:
+        return None
+
+
+def get_cookiecutter_project_path(seed_path):
+    # Check if we are within a notebooks sub path
+    path = seed_path
+    while not os.path.ismount(path):
+        if os.path.basename(path) == 'notebooks':
+            project_dir = os.path.dirname(path)
+            return project_dir
+        path = os.path.dirname(path)
+
+    # Check if we are at the root. And there is a src subdirectory.
+    if os.path.isdir(os.path.join(seed_path, 'src')):
+        return seed_path
     return None
 
 
-set_notebook_path(os.getcwd())
+def get_src_path(project_dir):
+    return os.path.join(project_dir, 'src') if project_dir else None
+
+
+infer_and_set_project_dir()
